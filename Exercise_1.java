c26dbd17f700f62bb10deeb770409a5983c44d0d
John Akech <j.akech@alustudent.com>
2025-05-15 15:07:57 +0200

    My first java exercise.

---
+++ Exercise_1.java
@@ -0,0 +1,10 @@
+// This is a simple Java program that prints a tongue twister to the console.
+class Exercise_1 {
+    // The main method is the entry point of any Java program.
+    public static void main(String[] args) {
+        // Print the tongue twister to the console.
+        // The System.out.println() method is used to print text to the console.
+        // The text inside the quotes is the message that will be printed.
+        System.out.println("How much wood would a woodchuck chuck if a woodchuck could chuck wood?");
+    }
+}
