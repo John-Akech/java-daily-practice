619c55ddf71ee5fb053bba786074445720bd1eaf
John Akech <j.akech@alustudent.com>
2025-05-15 15:04:12 +0200

    Java Initial commit.

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
+// This is a simple Java program that prints "Hello World, I love Java!" to the console.
 class Hello{
+    // The main method is the entry point of any Java program.
     public static void main(String[] args){
+        // Print the message to the console.
+        // The System.out.println() method is used to print text to the console.
+        // The text inside the quotes is the message that will be printed.
         System.out.println("Hello World, I love Java!");
     }
 }
